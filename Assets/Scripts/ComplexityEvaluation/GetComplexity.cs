10360c6c8a6039e7290b2590fb88ba9757d73b04
Cobra-117 <timothe.lacheroy@epitech.eu>
2023-03-23 11:38:05 +0100

    fix solution lenght finder

--- Assets/Scripts/ComplexityEvaluation/GetComplexity.cs
+++ Assets/Scripts/ComplexityEvaluation/GetComplexity.cs
@@ -8,15 +8,27 @@ public class GetComplexity : MonoBehaviour
 
     public int mazeWidth = 10;
     public int mazeLenght= 10;
+    bool debug = true;
+    int[,] maze;
     // Start is called before the first frame update
     void Start()
     {
         mazeAlgo.Generate(mazeWidth, mazeLenght);
-        int[,] maze = mazeAlgo.GetMaze();
+        maze = mazeAlgo.GetMaze();
         Debug.Log("Cyclomatic complexity: " + 
         GetCyclomaticComplexity(maze).ToString());
+        /*Debug.Log("Solution lenght: " + 
+        GetSolutionLenght(maze).ToString());*/
     }
 
+    void Update()
+    {
+        if (debug == true) {
+            Debug.Log("Solution lenght: " + 
+            GetSolutionLenght(maze).ToString());
+            debug = false;
+        }
+    }
     int GetCyclomaticComplexity(int[,] maze)
     {
         // E – N + 2, where E = number of edges, N = number of nodes
@@ -33,9 +45,58 @@ public class GetComplexity : MonoBehaviour
         return edges - nodes + 2;
     }
 
-    int GetSolutionLenght()
+    int GetSolutionLenght(int[,] maze)
     {
-        return 0;
+        return (int)GetDistanceMap(maze)[0, 0];
+    }
+
+    float[,] GetDistanceMap(int[,] maze)
+    {
+        float [,] distanceMap = new float
+        [maze.GetLongLength(0), maze.GetLongLength(1)];
+        for (int y = 0; y < maze.GetLongLength(0); y++) {
+            for (int x = 0; x < maze.GetLongLength(1); x++) {
+                distanceMap[y, x] = -1;
+            }
+        }
+
+        bool hasIndexed = true;
+        int currentDistance = 0;
+        distanceMap[maze.GetLongLength(0) -1, maze.GetLongLength(1) - 1] = 0;
+        while (hasIndexed == true) {
+            hasIndexed = false;
+            currentDistance += 1;
+            Debug.Log("Indexing: " + currentDistance.ToString());
+            for (int y = 0; y < maze.GetLongLength(0); y++) {   
+                for (int x = 0; x < maze.GetLongLength(1); x++) {
+                    if (distanceMap[y, x] != currentDistance -1)
+                        continue;
+                    //Debug.Log("Found end: ");
+                    if (!HasNorthEdge(maze[y, x]) && distanceMap[y - 1, x] == -1) {
+                        hasIndexed = true;
+                        distanceMap[y - 1, x] = currentDistance;
+                        Debug.Log("Indexed North: " + (y - 1).ToString() + " " + x.ToString());
+                    }
+                    if (!HasEastEdge(maze[y, x]) && distanceMap[y, x + 1] == -1) {
+                        hasIndexed = true;
+                        distanceMap[y, x + 1] = currentDistance;
+                        Debug.Log("Indexed East: " + (y).ToString() + " " + (x+1).ToString());
+                    }
+                    if (!HasSouthEdge(maze[y, x]) && distanceMap[y + 1, x] == -1) {
+                        hasIndexed = true;
+                        distanceMap[y + 1, x] = currentDistance;
+                        Debug.Log("Indexed South: " + (y+1).ToString() + " " + (x).ToString());
+                    }
+                    if (!HasWestEdge(maze[y, x]) && distanceMap[y, x - 1] == -1) {
+                        hasIndexed = true;
+                        distanceMap[y, x - 1] = currentDistance;
+                        Debug.Log("Indexed West: " + (y).ToString() + " " + (x-1).ToString());
+                    }
+                }
+            }
+            //return distanceMap;
+        }
+        return distanceMap;
     }
 
     bool HasNorthEdge(int tileValue) 
@@ -72,7 +133,7 @@ public class GetComplexity : MonoBehaviour
         || tileValue == (int)MazeAlgo.TileTypes.NS
         || tileValue == (int)MazeAlgo.TileTypes.NES
         || tileValue == (int)MazeAlgo.TileTypes.NESW
-        || tileValue == (int)MazeAlgo.TileTypes.NEW
+        || tileValue == (int)MazeAlgo.TileTypes.NSW
         || tileValue == (int)MazeAlgo.TileTypes.ES
         || tileValue == (int)MazeAlgo.TileTypes.ESW
         || tileValue == (int)MazeAlgo.TileTypes.SW)
@@ -88,8 +149,8 @@ public class GetComplexity : MonoBehaviour
         || tileValue == (int)MazeAlgo.TileTypes.NESW
         || tileValue == (int)MazeAlgo.TileTypes.EW
         || tileValue == (int)MazeAlgo.TileTypes.SW
-        || tileValue == (int)MazeAlgo.TileTypes.NES
-        || tileValue == (int)MazeAlgo.TileTypes.SW)
+        || tileValue == (int)MazeAlgo.TileTypes.NSW
+        || tileValue == (int)MazeAlgo.TileTypes.ESW)
             return true;
         return false;
     }
